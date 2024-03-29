a2d9815c7902e334ce9e8c9c74d48b387273ca03
Daria <dasha.tas97@gmail.com>
2024-03-29 12:41:25 +0200

    added handling for saving existing title and login and getting multiple rows from csv

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -2,20 +2,44 @@ import argparse
 import csv
 import password_generator as gen
 
-# Task doesn't state if title is unique.
-# Case when user tries to save creds for existing title is not handled.
-# For the same reason returning password by title returns first occurrence.
-
 csv_file = "data.csv"
 
 
 def pass_save(title, login, password):
     row_list = [title, login, password]
-    file_obj = open(csv_file, 'a', newline='')
-    csv_obj = csv.writer(file_obj, lineterminator='\n')
-    csv_obj.writerow(row_list)
-    file_obj.close()
-    print('Password for', title, 'saved!')
+    pass_exists = check_pass_exist(title, login)
+    if pass_exists:
+        print("Password for", title, "with login", login, "already exists. Do you want to update password? (Yes/No)")
+        answer = input()
+        if answer.lower() == 'yes':
+            file_obj = open(csv_file, 'r')
+            reader = csv.reader(file_obj)
+            rows_keep = [line for line in reader if
+                         line[0].lower().strip() != title.lower().strip() and line[1] != login]
+            file_obj = open(csv_file, 'w', newline='')
+            writer = csv.writer(file_obj, lineterminator='\n')
+            rows_keep.append(row_list)
+            for row in rows_keep:
+                writer.writerow(row)
+            print("Password for", title, "with login", login, "updated.")
+        else:
+            print("No changes saved.")
+    else:
+        file_obj = open(csv_file, 'a', newline='')
+        csv_obj = csv.writer(file_obj, lineterminator='\n')
+        csv_obj.writerow(row_list)
+        file_obj.close()
+        print('Password for', title, 'saved!')
+
+
+def check_pass_exist(title, login):
+    results = get_data(title)
+    if results:
+        for row in results:
+            if row['login'] == login:
+                return True
+    else:
+        return False
 
 
 def pass_generate(uppercase, symbols, numbers, duplicates, pass_length):
@@ -34,23 +58,34 @@ def pass_generate(uppercase, symbols, numbers, duplicates, pass_length):
 
 
 def get_pass(title):
-    file_obj = open(csv_file, 'r')
-    dict_reader = csv.DictReader(file_obj)
-    for row in dict_reader:
-        if row['title'].lower().strip() == title.lower().strip():
+    results = get_data(title)
+    print("You have saved passwords for ", len(results), title, "accounts:\n")
+    if results:
+        for row in results:
             for x in row:
                 print(x, row[x], sep=': ')
-            return
-    print("Password for", title, "not found!")
+            print('')
+    else:
+        print("Password for", title, "not found!")
+
+
+def get_data(title):
+    file_obj = open(csv_file, 'r')
+    dict_reader = csv.DictReader(file_obj)
+    results = []
+    for row in dict_reader:
+        if row['title'].lower().strip() == title.lower().strip():
+            results.append(row)
+    return results
 
 
 def get_all_titles():
     file_obj = open(csv_file, 'r')
     dict_reader = csv.DictReader(file_obj)
-    titles = []
+    titles = set()
     for col in dict_reader:
-        titles.append(col['title'])
-    if len(titles) == 0:
+        titles.add(col['title'])
+    if not titles:
         print('No saved passwords in manager.')
     else:
         print("You've saved passwords for following resources: ", ', '.join(titles))
